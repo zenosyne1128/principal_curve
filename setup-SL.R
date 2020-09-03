c34508cd98521bff11d2eed22dabd5fe67d58f61
zenosyne1128 <60611031+zenosyne1128@users.noreply.github.com>
2020-09-03 15:36:06 +0800

    add foreach and contininuation

--- setup-SL.R
+++ setup-SL.R
@@ -1,6 +1,7 @@
 library(readr)
 library(ggplot2)
-setwd('d:/Rfiles/principle_curves/stream_level/stream_level')
+library(rgl)
+setwd('e:/rmu_study/project/principal_curve')
 
 data = read.table('data.txt')
 
@@ -37,15 +38,71 @@ contour(data,nlevels = 45)
 numPoints = nrow(tib) 
 p = tib$h/sum(tib$h)
 
+set.seed(1)
 plotSample = tib[sample(numPoints, 5e4, replace = T, prob = p),]
 plot(plotSample$x,plotSample$y, pch = 20, col = "grey", cex = 0.1)
 
 source("pgh.R")
 source("principal_curve_KDE.R")
-library(rgl)
+
 kernel_sigma <- 10
                         
 sample = as.matrix(plotSample)
+sample = sample[,-3]
 
-pc_projection <- principal_curve_KDE(sample, sample, kernel_sigma,1)
+# -------------------------
+# foreach
+# ------------------------
+library(iterators)
+library(parallel)
+library(foreach)
+library(doParallel)
+
+# Real physical cores in the computer
+cores <- detectCores(logical=F)
+cl <- makeCluster(cores)
+registerDoParallel(cl, cores=cores)
+
+# split data by ourselves
+N <- nrow(sample)
+chunk.size <- N/cores
+
+#chunk.size <- new_number/cores
+system.time(
+  res1.p <- foreach(i=1:cores, .combine='rbind') %dopar%
+    {  # local data for results
+      res <- matrix(0, nrow=chunk.size, ncol=4)
+      # out put the new kernel_sigma in the third coordinate
+      for(x in ((i-1)*chunk.size+1):(i*chunk.size)) {
+        res[x - (i-1)*chunk.size,] <- principal_curve_KDE(sample, sample[x,], kernel_sigma,1,adaptive = F)
+      }# shoudn't run in row, the put in must be matrix
+      # return local results
+      res
+    }
+)
+
+stopImplicitCluster()
+stopCluster(cl)
+pc_projection <- res1.p[,1:2]
+
 points(pc_projection,col=rgb(0,0,1,0.5),cex=0.3)
+
+# ---------------------------
+# continuation
+# -----------------------------
+newmat <- matrix(0,nrow = nrow(data),ncol = ncol(data))
+for (i in 1:nrow(pc_projection)) {
+  j <- pc_projection[i,1]
+  k <- pc_projection[i,2]
+  newmat[j,k] <- 1
+}
+
+write.csv(newmat,file = 'E:\\rmu_study\\project\\skel2graph3d-matlab\\stream.csv',row.names = F,col.names = F)
+
+library(R.matlab)
+path <- ('e:/rmu_study/project/principal_curve/endpiont.mat')
+endpiont <- readMat(path)
+endpiont <- endpiont[[1]]
+endpiont[endpiont==0]=NA
+endpiont <- na.omit(endpiont)
+points(endpiont,col=rgb(0,0,1,0.5),cex=1)
